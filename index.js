75c63e046ddbf902453ecfac93d252bba703a99c
Nazimskz4 <93873488+Nazimskz4@users.noreply.github.com>
2022-02-28 18:51:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/*Below function is for Menu open and close*/
+
+var mobileWordlinks = document.getElementById("mobileWordlinks");
+function showMenu() {
+	mobileWordlinks.style.right = "0";
+}
+
+function hideMenu() {
+	mobileWordlinks.style.right = "-60rem";
+}
